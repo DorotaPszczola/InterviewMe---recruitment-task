d19f9690b768df8156e9cd40b0ff88ddd2de1495
Dorota Pszczola <d.a.pszczola@gmail.com>
2020-02-25 00:37:48 +0100

    add navbar, hamburger, hamburger-menu, section Features (RWD)

---
+++ main.js
@@ -0,0 +1,16 @@
+const $hamburger = document.querySelector(".hamburger");
+const $mobileMenu = document.querySelector('.menu-hamburger');
+
+const activateHamburger = () => {
+    $hamburger.classList.toggle('hamburger-active');
+    $mobileMenu.classList.toggle('menu-hamburger-active');
+};
+// const showMobileMenu = () => {
+// }
+
+$hamburger.addEventListener('click', activateHamburger);
+
+
+
+
+
