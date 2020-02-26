a4415c50b53a5b8b814b0b61ce05d8a4fcc32bc9
Dorota Pszczola <d.a.pszczola@gmail.com>
2020-02-26 04:36:35 +0100

    add js to slider and form

--- main.js
+++ main.js
@@ -11,5 +11,59 @@ const activateHamburger = () => {
 $hamburger.addEventListener('click', activateHamburger);
 
 
+// slider //
+
+let i = 0;
+    const images = document.getElementsByClassName("item-image");
+let time = 1000;
+
+function changeImg(){
+            images[i].classList.add("active-item");
+            images[i].classList.remove("faded-item-right");
+            images[Math.abs(i-1)].classList.add("faded-item-right");
+            images[Math.abs(i-1)].classList.remove("faded-item-left");
+            images[Math.abs(i-2)].classList.add("faded-item-left");
+            images[Math.abs(i-2)].classList.remove("active-item");
+            if (i < images.length - 1) {
+                i++;
+
+            } else {
+                i = 0;
+            }
+            setTimeout("changeImg()", time)
+}
+window.onload = changeImg;
+
+
+
 // contact form //
 
+(function() {
+	function toJSONString( form ) {
+		let obj = {};
+		const elements = form.querySelectorAll( "input, select" );
+		for( let i = 0; i < elements.length; ++i ) {
+		    const element = elements[i];
+			const name = element.name;
+			const value = element.value;
+
+			if( name ) {
+				obj[ name ] = value;
+			}
+		}
+		return JSON.stringify( obj );
+	}
+
+	document.addEventListener( "DOMContentLoaded", function() {
+		const form = document.getElementById( "contact-form" );
+		const output = document.getElementById( "output" );
+		form.addEventListener( "submit", function( e ) {
+			e.preventDefault();
+			let json = toJSONString( this );
+			output.innerHTML = json;
+
+		}, false);
+
+	});
+
+})();
